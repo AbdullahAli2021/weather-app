7e29bd3d63051038bfc3063a4844b2f627f43a0a
Abdulah Ali <w0782917@myscc.ca>
2022-03-05 17:45:05 -0500

    add the css and datebuilder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ const api = {
 }
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
       <main>
@@ -13,10 +24,12 @@ function App() {
             type='text'
             className='search-bar'
             placeholder='Search ...'
-          />
-          
-        
-        </div>  
+          />        
+        </div>
+        <div className="location-box">
+          <div className="location">Windsor, CA</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div> 
       </main>
       
     </div>
