77c3dff91d4c8d79c1a983c056f111c25fecafd3
Abdulah Ali <w0782917@myscc.ca>
2022-03-05 18:27:16 -0500

    searcx input query

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result)
+        });
+    }
+  }
+
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -20,11 +36,14 @@ function App() {
   return (
     <div className="app">
       <main>
-        <div className="searchbox">
+        <div className="search-box">
           <input
             type='text'
             className='search-bar'
             placeholder='Search ...'
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />        
         </div>
         <div className="location-box">
