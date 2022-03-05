f895ce0ff27ab691f96f1d27fef7c3aa60bbd230
Abdulah Ali <w0782917@myscc.ca>
2022-03-05 14:54:15 -0500

    search input and img

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 const api = {
   key: '2b810a1878c74d39c25cb97d2d29f529',
   base: "https://api.openweathermap.org/data/2.5/"
-
 }
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <main>
+        <div className="searchbox">
+          <input
+            type='text'
+            className='search-bar'
+            placeholder='Search ...'
+          />
+          
+        
+        </div>  
+      </main>
       
     </div>
   );
